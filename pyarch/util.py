4972ecacb64ca5d9796c5d7b6b97d573c1e4af22
Elizabeth Myers <elizabeth.jennifer.myers@gmail.com>
2023-01-21 02:33:25 -0800

    util: add double utility functions

--- pyarch/util.py
+++ pyarch/util.py
@@ -14,3 +14,21 @@ def set_word_byte(num, byte, val):
 
 def get_word_byte(num, byte):
     return (num >> ((3 - byte) * 8)) & 0xff
+
+def set_double_byte(num, byte, val):
+    mask_table = [
+        0xffffffffffffff00,
+		0xffffffffffff00ff,
+		0xffffffffff00ffff,
+		0xffffffff00ffffff,
+		0xffffff00ffffffff,
+		0xffff00ffffffffff,
+		0xff00ffffffffffff,
+		0x00ffffffffffffff,
+    ]
+    val &= 0xff
+    byte = 7 - byte
+    return (mask_table[byte] & num) | (val << (byte * 8))
+
+def get_double_byte(num, byte):
+    return (num >> ((7 - byte) * 8)) & 0xff
